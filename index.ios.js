ada5865590e558bfb9fda562daf712f3a20d8a08
Jack Cutting <me@jackcutting.co.uk>
2016-11-16 18:07:54 +0000

    Initial Commit

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+// Index.ios.js - place code in here for iOS
+
+//  Import library to help create a component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
